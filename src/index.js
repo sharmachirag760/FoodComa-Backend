c6b4c85bf5eed9a6ccd8bcac33bb8761a8c4d131
chirag sharma <sharmachirag760@gmail.com>
2024-07-13 15:11:56 +0530

    tested user schema

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 // const bodyParser = require('body-parser')
 const serverConfig = require('./Config/serverConfig');
 const connectDB = require('./Config/dbConfig');
+// const User = require('./schema/userSchema')
 const app = express();
 app.use(express.json({extended : true}))
 app.use(express.text({extended : true}))
@@ -15,5 +16,13 @@ app.post("/ping",(req,res)=>{
 app.listen(serverConfig.port, async () => {
     await connectDB();
     console.log(`server started at ${serverConfig.port} port `)
-
+//    const newUser = await User.create({
+//      firstName: "chirag",
+//      lastName: "sharma",
+//      mobileNumber: "7737003298",
+//      email: 'ab@gmail.com',
+//      password: "12345678"
+//    })
+//    console.log("value updated")
+//    console.log(newUser) 
 })
