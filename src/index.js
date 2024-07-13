aa88fa07caf816fab88a8a3e1e881b96f4e7055d
chirag sharma <sharmachirag760@gmail.com>
2024-07-13 11:04:37 +0530

    express ready to use

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const serverConfig = require('./Config/serverConfig');
 const connectDB = require('./Config/dbConfig');
 const app = express();
-app.use(bodyParser.json())
-app.use(bodyParser.text())
-app.use(bodyParser.urlencoded())
+app.use(express.json({extended : true}))
+app.use(express.text({extended : true}))
+app.use(express.urlencoded({extended : true}))
 
+app.post("/ping",(req,res)=>{
+    console.log(req.body)
+    return res.json({message : "pong"})
+})
 
 app.listen(serverConfig.port, async () => {
     await connectDB();
